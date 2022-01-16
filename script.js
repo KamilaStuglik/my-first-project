1720c010a7a36177293339feed16e0145df8c8c3
KamilaStuglik <77364670+KamilaStuglik@users.noreply.github.com>
2022-01-16 23:01:03 +0100

    Copied from SandBox

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-alert("Hello Git yay yay")
+let dateAndTime = document.querySelector("#current-time");
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+dateAndTime.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayWeather(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#aura").innerHTML = response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "c064a1d958e6c2a52bab92fa47962081";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+searchCity("Oslo");
+
+let searchForm = document.querySelector("#city-form");
+searchForm.addEventListener("submit", handleSubmit);
